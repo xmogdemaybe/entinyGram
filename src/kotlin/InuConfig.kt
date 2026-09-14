4fad8fd9c85456f4932c6c4762244dcd4a5d445c
xmogdemaybe <xmogdelegenda@gmail.com>
2026-09-14 11:36:53 +0400

    interesno? 1189

--- src/kotlin/InuConfig.kt
+++ src/kotlin/InuConfig.kt
@@ -1186,6 +1186,9 @@ object InuConfig {
     val DISABLE_DRAFT_UPLOAD = BoolItem("disable_draft_upload", false)
 
     @JvmField
+    val ROUND_CAMERA_60FPS = BoolItem("round_camera_60fps", true)
+    
+    @JvmField
     val ROUND_DEFAULT_CAMERA = IntItem("round_default_camera", 1) // 1=Front, 2=Rear, 3=Ask
 
     @JvmField
